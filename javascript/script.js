d089b54c403f7d0cca54427c7fd8dc6342386de4
Joacim Hammarström <joacimhammarstrom@gmail.com>
2019-11-30 20:22:32 +0100

    fifth commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,18 @@
 // STARTAR VALIDERING
 // går igenom alla input-fält
-const inputs = document.getElementsByTagName("input");
+var inputs = document.getElementsByTagName("input");
 const radio = document.getElementsByName("gender");
 
-// EVENTS
+inputEvents();
+
+function inputEvents() {
+    // EVENTS
 for (var i = 0; i < inputs.length; i++) {
     // FOCUS = När man väljer ett fält sätts en function som tar bort alerts för det fältet
     inputs[i].onfocus = deleteAlerts; //tar bort meddelanden för inputs
 
     if (inputs[i].name === "gender"){  //tar bort meddelanden för radio //Ska radio användas istället för gender??????
         inputs[i].onfocus = deleteGenderAlerts;}
-
-    document.getElementById('customerGroup').onfocus = deleteCustomerAlerts; //tar bort meddelanden för customerGroup
-    document.getElementById('terms').onfocus = deleteTermsAlerts; //tar bort meddelanden för terms
     
     // BLUR = När man lämnar ett fält sätts doThisOnBlur igång på det fältet
     if (inputs[i].name !== "gender" && inputs[i].id !== "terms") {
@@ -21,52 +21,65 @@ for (var i = 0; i < inputs.length; i++) {
     function doThisOnBlur() {
         let id = this.id;
         let value = this.value;
-        let validReturn = validate(id, value);
+        let validReturn = inputValidate(id, value);
         alert(validReturn, id);}
 }
+}
+
+document.getElementById('customerGroup').onfocus = deleteCustomerAlerts; //tar bort meddelanden för customerGroup
+document.getElementById('terms').onfocus = deleteTermsAlerts; //tar bort meddelanden för terms
+
 
 // SUBMIT = När man klickar på submit startar en validering av alla input-fält
 document.getElementById("btnSubmit").onclick = submit;
+var errors;
 
-function submit(event) {
-  event.preventDefault();
+function submit() {
+       event.preventDefault();
 
-    var errors = 0
+    errors = 0;
+    // Tar varje inputs id (förutom radio och terms) och testar det i validate
+    inputLoop();
 
-  // Tar varje inputs id (förutom radio och terms) och testar det i validate
-  for (var e = 0; e < inputs.length; e++) {
-    if (inputs[e].name !== "gender" && inputs[e].id !== "terms") {
-        let id = inputs[e].id;
-        let value = inputs[e].value;
-        let validReturn = validate(id, value);
-        alert(validReturn, e);     // Meddelar om fel har hittats eller inte
+    if (document.contains(document.getElementById("regForm"))) {
+        // Meddelar om någon radiobutton är checked eller inte
+        genderAlerts(checkRadio);
+        if (checkRadio() !== true) { errors++}
 
-        if (validReturn !== true) { errors++}
-    }}
+        // Meddelar om någon select har blivit vald
+        customerAlerts(checkSelect);
+        if (checkSelect() !== true) { errors++}
 
-    // Meddelar om någon radiobutton är checked eller inte
-    genderAlerts(checkRadio);
-
-    if (checkRadio() !== true) { errors++}
-
-    // Meddelar om någon select har blivit vald
-    customerAlerts(checkSelect);
-
-    if (checkSelect() !== true) { errors++}
-
-    // Meddelar om någon select har blivit vald
-    termsAlerts(checkTerms);
-
-    if (checkTerms() !== true) { errors++}
-
-    if (errors === 0) {
-        document.getElementById("regForm").remove();
-        addInput();
-        // window.location.href = "someOtherFile.html";
+        // Meddelar om någon select har blivit vald
+        termsAlerts(checkTerms);
+        if (checkTerms() !== true) { errors++}
     }
 
     console.log(`There is something wrong in ${errors} input fields`)
+
+        if ((errors === 7) && (document.contains(document.getElementById("regForm"))))
+        {
+            document.getElementById("regForm").remove();
+            addLogin();
+        }
+        if ((errors === 1) && !(document.contains(document.getElementById("regForm"))))
+        {
+            document.getElementById('formContainer').remove();
+            youAreLoggedIn();
+        }
 }
+
+function inputLoop() {
+    for (var e = 0; e < inputs.length; e++) {
+      if (inputs[e].name !== "gender" && inputs[e].id !== "terms") {
+          let id = inputs[e].id;
+          let value = inputs[e].value;
+          let validReturn = inputValidate(id, value);
+          alert(validReturn, e);     // Meddelar om fel har hittats eller inte
+  
+          if (validReturn !== true) { errors++}
+      }}
+  }
 
 // TESTAR VILLKOR
     // Kollar om nån radiobutton är checked
@@ -99,86 +112,82 @@ function submit(event) {
         return re2.test(password);
         }
 
-function validate(id, value) {
+function inputValidate(id, value) {
 
     function length(i) {
         return value.length < i;
         }
 
-  switch (id) {
-    case "firstName":
-      if (length(2)) {
-        return "Please enter your first name.";
-      } else return true;
+    switch (id) {
+        case "firstName":
+        if (length(2)) {
+            return "Please enter your first name.";
+        } else return true;
 
-    case "lastName":
-      if (length(2)) {
-        return "Please enter your last name.";
-      } else return true;
+        case "lastName":
+        if (length(2)) {
+            return "Please enter your last name.";
+        } else return true;
 
-    case "email":
-      if (length(5)) {
-        return "Please insert an email.";
-      } else {
-        if (validateEmail(value)) {
-          return true;
+        case "email":
+        if (length(5)) {
+            return "Please insert your email.";
         } else {
-          return "It seems this is not a valid email. Please make sure it is correct.";
-        }
-      }
+            if (validateEmail(value)) {
+            return true;
+            } else {
+            return "It seems this is not a valid email. Please make sure it is correct.";
+            }}
 
-    case "password":
-      if (length(8)) {
-        return "Please choose a password at least 8 letters long.";
-      } else {
-        if (validatePassword(value)) {
-          return true;
+        case "password":
+        if (length(8)) {
+            return "Please insert a password at least 8 letters long.";
         } else {
-          return "The password must include at least one letter and one number";
-        }
-      }
+            if (validatePassword(value)) {
+            return true;
+            } else {
+            return "The password must include at least one letter and one number";
+            }}
 
-    case "message":
-      if (length(3)) {
-        return "Please enter a message";
-      } else return true;
-    default:
+        case "message":
+        if (length(3)) {
+            return "Please enter a message";
+        } else return true;
+        default:
   }
 }
 
 // SKICKA UT OCH TA BORT FELMEDDELANDEN
 // Raderar tidigare felmeddelanden
-function deleteAlerts() {
-    let elements = this.parentNode.getElementsByClassName("invalid-tooltip");
-    
+
+function removeAll(elements) {
     while (elements.length > 0) {
     elements[0].parentNode.removeChild(elements[0]);
-    }
-}
-function deleteGenderAlerts() {
-    if (document.contains(document.getElementById("genderAlert"))) {
-        document.getElementById('genderAlert').remove();
-    }  
-        for (var e = 0; e < radio.length; e++) {
-            radio[e].classList.remove("is-invalid");
-    }
+}}
+
+function deleteAlerts() {
+    let elements = this.parentNode.getElementsByClassName("invalid-tooltip");
+    removeAll(elements);
 }
 
+function deleteGenderAlerts() {
+    let elements = document.getElementById('genderAlert').parentNode.getElementsByClassName("invalid-tooltip");
+    removeAll(elements);
+    for (var e = 0; e < radio.length; e++) {
+    radio[e].classList.remove("is-invalid");
+}}
+
 function deleteCustomerAlerts() {
-    if (document.contains(document.getElementById('customerAlert'))) {
-        document.getElementById('customerAlert').remove();
-    }  
-        document.getElementById('customerGroup').classList.remove("is-invalid");
+    let elements = document.getElementById('customerAlert').parentNode.getElementsByClassName("invalid-tooltip");
+    removeAll(elements);
+    document.getElementById('customerGroup').classList.remove("is-invalid");
 }
 
 function deleteTermsAlerts() {
-    if (document.contains(document.getElementById('termsAlert'))) {
-        document.getElementById('termsAlert').remove();
-    }  
-        document.getElementById('terms').classList.remove("is-invalid");
+    let elements = document.getElementById('termsAlert').parentNode.getElementsByClassName("invalid-tooltip");
+    removeAll(elements);
+    document.getElementById('terms').classList.remove("is-invalid");
 }
-
-
 
 // Lägger till felmeddelanden och ändrar färger på fält
 function alert(validReturn, i) {
@@ -193,8 +202,7 @@ function alert(validReturn, i) {
   } else {
     inputs[i].classList.remove("is-invalid");
     inputs[i].classList.add("is-valid");
-  }
-}
+}}
 
 function genderAlerts(x) {
     //   Om ingen radio-button är checked
@@ -213,9 +221,7 @@ function genderAlerts(x) {
         else {
             for (var e = 0; e < radio.length; e++) {
             radio[e].classList.add("is-valid");    
-            }
-        }
-}
+}}}
 
 function customerAlerts(x) {
     //   Om ingen alternativ är valt
@@ -232,9 +238,7 @@ function customerAlerts(x) {
         else {
             for (var e = 0; e < radio.length; e++) {
                 document.getElementById('customerGroup').classList.add("is-valid");    
-            }
-        }
-}
+}}}
 
 function termsAlerts(x) {
     //   Om ingen alternativ är valt
@@ -251,29 +255,48 @@ function termsAlerts(x) {
         else {
             for (var e = 0; e < radio.length; e++) {
                 document.getElementById('terms').classList.add("is-valid");    
-            }
-        }
-}
+}}}
 
-
-
-
-function addInput() {
-    const div = document.createElement('div');
+function addLogin() {
+    const div = document.createElement('form');
   
-    div.className = 'input';
+    div.className = 'login';
   
     div.innerHTML = `
-    <form autocomplete="off" id="logInForm">
+    <div class="m-4">You are now registred!</div>
+    <form autocomplete="off" id="logInForm" class="m-5">
     <!-- EMAIL & PASSWORD -->
     <div class="form-group col-md-6">
-            <input id="logInEmail" type="text" class="form-control" placeholder="Email">
+            <input id="email" type="text" class="form-control" placeholder="Email">
         </div>
         <div class="form-group col-md-6">
-            <input id="logInPassword" type="password" class="form-control" placeholder="Password">
+            <input id="password" type="password" class="form-control" placeholder="Password">
+        </div>
+        <div class="form-group col-md-6">
+            <button id="btnLogin" type="submit" class="btn btn-primary">Login</button>
         </div>
     </form>
     `;
   
+    document.getElementById('formContainer').appendChild(div);
+
+    inputs = document.getElementsByTagName("input");
+    inputEvents();
+
+    document.getElementById("btnLogin").addEventListener("click", function(event){
+        event.preventDefault();
+        submit();
+})}
+
+function youAreLoggedIn() {
+    const div = document.createElement('div');
+    div.className = 'LoggedIn';
+  
+    div.innerHTML = `
+    <div class="m-4">You are now logged in!</div>
+    `;
+  
     document.getElementById('containerId').appendChild(div);
-  }
+}
+
+
